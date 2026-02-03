24dcd33e9dcfdd73d3cefefdc9221b3ee0ffbb59
rinshan77 <175357996+rinshan77@users.noreply.github.com>
2026-02-03 14:54:47 +0100

    Build site to docs for GitHub Pages

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+REPO_NAME="staticsitegenerator"
+python3 src/main.py "/${REPO_NAME}/"
+
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-from copy_static import copy_directory 
-from generate_page import generate_pages_recursive
+# src/main.py
+import sys
 
+from copy_static import copy_directory
+from generate_page import generate_pages_recursive 
 
 def main():
-    copy_directory("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
+    out_dir = "docs"
+
+    copy_directory("static", out_dir)
+    generate_pages_recursive("content", "template.html", out_dir, "/staticsitegenerator/")
 
 if __name__ == "__main__":
     main()
